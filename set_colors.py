f3af0509b746bd3d4ee0d1fe9c05ba51fd88fbb8
CC <c.c.lam.2@umail.leidenuniv.nl>
2019-07-27 15:35:11 +0200

    Added darcula theme Made set_colors script take in themes as parameter Made copying of icon version independent

--- set_colors.py
+++ set_colors.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 
-# usage: path to _code_browser.tcd (in your home directory)
+# usage: python set_colors.py [toolsPath] [theme file]
 
 import os
 import json
 import sys
-from os.path import expanduser
 from xml.dom import minidom
 from shutil import copyfile
 
-
-def set_color(xmldoc, category, name, color):
+def set_color(xmldoc, cat, name, color):
     found = False
     for category_tag in xmldoc.getElementsByTagName("CATEGORY"):
         if category_tag.getAttribute("NAME") == category:
@@ -35,46 +33,30 @@ def set_color(xmldoc, category, name, color):
     category_tag.appendChild(wrapper)
     wrapper.appendChild(state)
 
-
 if __name__ == "__main__":
-    code_browser_tcd = sys.argv[1]
+    toolsDir = sys.argv[1]
+    themeName = sys.argv[2]
 
-    xmldoc = minidom.parse(sys.argv[1])
+    xmldoc = minidom.parse(os.path.join(toolsDir, "_code_browser.tcd"))
 
-    """
-    # debug
-    for category in xmldoc.getElementsByTagName("CATEGORY"):
-        print('"%s": {' % category.getAttribute("NAME"))
-        for tag in category.getElementsByTagName("WRAPPED_OPTION"):
-            if tag.getAttribute("CLASS") == "ghidra.framework.options.WrappedColor":
-                state = tag.getElementsByTagName("STATE")[0]
-                print('    "%s": %d,' % (tag.getAttribute("NAME"), int(state.getAttribute("VALUE"))))
-        print("},")
-    """
-
-    all_colors = json.loads(open("darknight", "r").read())
+    theme = json.loads(open(themeName, "r").read())
 
     for tag in xmldoc.getElementsByTagName("WRAPPED_OPTION"):
         if tag.getAttribute("CLASS") == "ghidra.framework.options.WrappedColor":
             tag.parentNode.removeChild(tag)
 
-    for category, colors in all_colors.items():
+    for category, colors in theme.items():
         for name, color in colors.items():
             set_color(xmldoc, category, name, color)
 
     tool = xmldoc.getElementsByTagName("TOOL")[0]
-    tool.setAttribute("TOOL_NAME", "CodeBrowserDarkNight")
+    tool.setAttribute("TOOL_NAME", "CodeBrowser%s" % themeName)
 
-    dragon_path = expanduser("~") + "/.ghidra/.ghidra-9.0/tools/black_dragon.png"
+    dragon_path = os.path.join(toolsDir, "black_dragon.png")
     icon = xmldoc.getElementsByTagName("ICON")[0]
     icon.setAttribute("LOCATION", dragon_path)
     copyfile("black_dragon.png", dragon_path)
     print("-> black dragon icon to %s" % dragon_path)
 
-    d = os.path.dirname(sys.argv[1])
-
-    filename_out = "%s/_code_browser_dark_night.tcd" % d
-    open(filename_out, "w+").write(xmldoc.toprettyxml(indent="    ", newl="\n"))
-
-    print("-> %s" % filename_out)
-    print("now open ghidra and select the black dragon")
+    fileName = os.path.join(toolsDir, "_code_browser_%s.tcd" % themeName)
+    open(fileName, "w+").write(xmldoc.toprettyxml(indent="    ", newl="\n"))
